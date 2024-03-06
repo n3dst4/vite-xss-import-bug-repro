230a0adb43af319d1efe65fb88757f268c7a9c73
Neil de Carteret <git@lumphammer.net>
2024-03-06 11:52:57 +0000

    more buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,86 @@
 import { useCallback, useState } from "react";
 import "./App.css";
-// import xss from "xss";
+import xss, { FilterXSS } from "xss";
 
-const dirty = "<script>alert('xss')</script>";
-
-const createXss = async () => {
-  const {
-    FilterXSS,
-    whiteList: defaultXssWhitelist,
-    escapeAttrValue,
-  } = await import("xss");
-
-  // build a custom shitelist for xss that adds "style" to the allowed attributes
-  // for everything
-  const newWhitelist = Object.fromEntries(
-    Object.entries(defaultXssWhitelist).map(([tag, attrList = []]) => [
-      tag,
-      [...attrList, "style"],
-    ])
-  );
-
-  // copilot said this but it does not work
-  // newWhitelist["*"] = ["style"];
-
-  // custom xss to allow style attributes and allow images with src attributes.
-  // Yes, it's not ideal XSS, but then again this is a collaborative, trusted
-  // environment.
-  const xss = new FilterXSS({
-    whiteList: newWhitelist,
-    onTagAttr: function (tag, name, value) {
-      if (tag === "img" && name === "src") {
-        // escape its value using built-in escapeAttrValue function
-        return name + '="' + escapeAttrValue(value) + '"';
-      }
-    },
-  });
-  return xss;
-};
+const dirty =
+  "foo bar <section class='corge'>baz</section> qux <img src='whatever' />";
 
 function App() {
   const [text, setText] = useState(dirty);
 
-  // const handleClean = useCallback(() => {
-  //   const cleaned = xss(dirty);
-  //   setText(cleaned);
-  // }, []);
-
+  // reset the tool
   const handleReset = useCallback(() => {
     setText(dirty);
   }, []);
 
-  const handleClean2 = useCallback(async () => {
-    const xss = await createXss();
+  // cleant the text using a
+  const handleCleanStandardDefaultImport = useCallback(async () => {
+    const cleaned = xss(dirty);
+    setText(cleaned);
+  }, []);
+
+  const handleCleanStandardNamedImport = useCallback(async () => {
+    const xss = new FilterXSS({});
+    const cleaned = xss.process(dirty);
+    setText(cleaned);
+  }, []);
+
+  const handleCleanDynamicDefaultImport = useCallback(async () => {
+    const xss = (await import("xss")).default;
+    const cleaned = xss(dirty);
+    setText(cleaned);
+  }, []);
+
+  const handleCleanDynamicNamedImport = useCallback(async () => {
+    const { FilterXSS } = await import("xss");
+    const xss = new FilterXSS({});
     const cleaned = xss.process(dirty);
     setText(cleaned);
   }, []);
 
   return (
     <>
-      <h1>Text</h1>
-      <pre>{text}</pre>
+      <p>
+        Below is some text that we want to clean. There are four buttons below.
+      </p>
+      <p>Standard default import, aka `import x from y;`</p>
+      <p>
+        Standard named import, aka `import {"{"}x{"}"} from y;`
+      </p>
+      <p>Dynamic default import, aka `await import(x).default;` </p>
+      <p>Dynamic named import, aka `await import(x).y;` </p>
+      <p>
+        <b>All of these methods work in Vite 5.0. </b>
+      </p>
+      <p>
+        <b>
+          All of these methods work in Vite 5.1 when building statically (vite
+          build)
+        </b>
+      </p>
+      <p>
+        <b>
+          But in Vite 5.1.x, in dev mode (vite dev), the final one (dynamic
+          named import) does not work. The named import is undefined.
+        </b>
+      </p>
+      <pre style={{ border: "1px solid currentColor", padding: "2em" }}>
+        {text}
+      </pre>
       <div>
         <button onClick={handleReset}>Reset</button>
-        {/* <button onClick={handleClean}>Clean</button> */}
-        <button onClick={handleClean2}>Clean (dynamic import)</button>
+        <button onClick={handleCleanStandardDefaultImport}>
+          Clean (standard default import)
+        </button>
+        <button onClick={handleCleanStandardNamedImport}>
+          Clean (standard named import)
+        </button>
+        <button onClick={handleCleanDynamicDefaultImport}>
+          Clean (dynamic default import)
+        </button>
+        <button onClick={handleCleanDynamicNamedImport}>
+          Clean (dynamic named import)
+        </button>
       </div>
     </>
   );
